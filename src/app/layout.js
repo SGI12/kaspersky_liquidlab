ddee588615bc6b3a624371701a91f5c21de96cb6
Daniil Zaretskiy <zaretskiy03@gmail.com>
2024-04-04 00:35:17 +0300

    fixed loading bar and add validation

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 
+
 const poppins = Poppins({subsets: ['latin'], weight: '400'})
 export const metadata = {
   title: "LiquidLab",
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      
       <body className={poppins.className}>{children}</body>
     </html>
   );
